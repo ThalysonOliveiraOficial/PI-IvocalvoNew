a3daddb2cb9b3c62d12cb5c28c1a491fd7916a7f
ivodesouzabarbosa <ivosouzabarbosa@gmail.com>
2024-10-24 00:55:19 -0300

    pontos multilayer

--- Assets/Multipl/GameContaControl.cs
+++ Assets/Multipl/GameContaControl.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameContaControl : MonoBehaviour
 {
     public List<int> _respList;
     public List<blocoNumero> _blocoNueroList;
+    public int NumberPlayers;
+    public int[] pontosPlayer;
+    public TextMeshProUGUI[] textMeshProUGUI;
     // Start is called before the first frame update
     private void Start()
     {
         Invoke("SetblocoNumber", 1);
+        textMeshProUGUI[0].text = "Pontos 0";
+        textMeshProUGUI[1].text = "Pontos 0";
     }
 
     // Update is called once per frame
--- Assets/Multipl/Move_multi.cs
+++ Assets/Multipl/Move_multi.cs
@@ -11,9 +11,25 @@ public class Move_multi : MonoBehaviour
     public TextMeshPro _textPlayer;
     public blocoNumero _blocoNumero;
     public Conta _conta;
+    GameContaControl _contaControl;
+    public int _playerN;
+    public Sprite[] SpriteIMG;
+    SpriteRenderer _spriteRenderer;
     void Start()
     {
         _rigidbody2 = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _contaControl = Camera.main.GetComponent<GameContaControl>();
+        _contaControl.NumberPlayers++;
+        _playerN = _contaControl.NumberPlayers;
+        if (_playerN == 1)
+        {
+            _spriteRenderer.sprite = SpriteIMG[0];
+        }
+        else
+        {
+            _spriteRenderer.sprite = SpriteIMG[1];
+        }
     }
 
     // Update is called once per frame
@@ -41,6 +57,27 @@ public class Move_multi : MonoBehaviour
             {
                 
                 _conta.ContaSet("" + _blocoNumero._numeroBloco);
+                if (_playerN == 1)
+                {
+                    _contaControl.pontosPlayer[0]++;
+                    _contaControl.textMeshProUGUI[0].text = "Pontos " + _contaControl.pontosPlayer[0];
+                }
+                else
+                {
+                    _contaControl.pontosPlayer[1]++;
+                    _contaControl.textMeshProUGUI[1].text = "Pontos " + _contaControl.pontosPlayer[1];
+                }
+
+                if (_contaControl.pontosPlayer[1]==2)
+                {
+                    Debug.Log("para o jogo, apresenta tela de jogador 1 vencedor");
+                }
+                else if (_contaControl.pontosPlayer[0] == 2)
+                {
+                    Debug.Log("para o jogo, apresenta tela de jogador 2 vencedor");
+                }
+                
+
             }
             else
             {
